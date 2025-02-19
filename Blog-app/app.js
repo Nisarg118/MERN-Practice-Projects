c7692e5323e7541693b77b78754da6a6bf7831cb
Nisarg118 <nisargbhamat12@gmail.com>
2025-02-19 22:54:16 +0530

    Blog-app

---
+++ Blog-app/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+require("dotenv").config();
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Mongodb Connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is working");
+});
